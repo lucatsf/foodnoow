ccb1e1c042652c3dc4ac33aaeb96df38a1fb490b
Lucas Torres 🥷 <lucatsf@icloud.com>
2023-11-23 01:55:10 -0300

    feat: add more allowedOrigins

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,14 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 const allowedOrigins = process.env.NEXT_NODE_ENV === 'production' 
-  ? ['foodnoow.com.br', 'https://foodnoow.com.br', 'https://www.foodnoow.com.br']
+  ? [
+    'foodnoow.com.br',
+    'https://foodnoow.com.br',
+    'https://www.foodnoow.com.br',
+    'foodnoow.vercel.app',
+    'https://foodnoow-torresfelicio.vercel.app/',
+    'https://foodnoow-git-main-torresfelicio.vercel.app/'
+  ]
   : ['localhost:3000', 'http://localhost:3000'];
 
 export async function middleware(req) {

a8587d596ddcb48c203d97518d39f4e503b31d0a
Lucas Torres 🥷 <lucatsf@icloud.com>
2023-11-26 13:48:02 -0300

    fix: password validation and add email validation

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,4 +1,3 @@
-import {User} from "@/models/User";
 import bcrypt from "bcrypt";
 import { checkLimiter } from "../config/limiter";
 import { response } from "@/libs/response";
@@ -9,20 +8,22 @@ export async function POST(req) {
   const body = await req.json();
   const pass = body.password;
   if (!pass?.length || pass.length < 5) {
-    new Error('A senha deve ter no mínimo 5 caracteres');
+    throw new Error('A senha deve ter no mínimo 5 caracteres');
   }
   if (!body?.email || body?.email === '') {
-    new Error('O email é obrigatório');
+    throw new Error('O email é obrigatório');
   }
   if (!body?.name || body?.name === '') {
-    new Error('O nome é obrigatório');
+    throw new Error('O nome é obrigatório');
   }
-
+  const userService = new UserService();
+  const userExists = await userService.find({email: body.email});
+  if (userExists) {
+    throw new Error('Este email já está cadastrado');
+  }
   const notHashedPassword = pass;
   const salt = bcrypt.genSaltSync(10);
   body.password = bcrypt.hashSync(notHashedPassword, salt);
-
-  const userService = new UserService();
   const createdUser = await userService.create(body);
   return response(createdUser, {req});
 }

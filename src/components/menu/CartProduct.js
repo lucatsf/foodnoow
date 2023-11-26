08c586a7aa38a737b44aa94d82f704fbb15a389d
Lucas Torres 🥷 <lucatsf@icloud.com>
2023-11-26 18:21:21 -0300

    refactor: CartProduct component to handle optional properties

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -5,29 +5,29 @@ import Image from "next/image";
 
 export default function CartProduct({indexProduct,product,onRemove}) {
   return (
-    <div className="flex items-center gap-4 border-b py-4">
+    <div className="flex items-center gap-4 border-b py-4" key={indexProduct}>
       <div className="w-24">
-        <Image width={240} height={240} src={product.image || '/not-image.png'} alt={product.name} />
+        <Image width={240} height={240} src={product?.image || '/not-image.png'} alt={product?.name} />
       </div>
       <div className="grow">
         <h3 className="font-semibold">
-          {product.name}
+          {product?.name}
         </h3>
-        {product.size && (
+        {product?.size && (
           <div className="text-sm">
-            Tamanho: <span>{product.size.name}</span>
+            Tamanho: <span>{product?.size?.name}</span>
           </div>
         )}
-        {product.extras?.length > 0 && (
+        {product?.extras?.length > 0 && (
           <div className="text-sm text-gray-500">
-            {product.extras.map(extra => (
-              <div key={extra.name}>{extra.name} {formatFromMoney(extra?.price)}</div>
+            {product?.extras.map(extra => (
+              <div key={extra?.name}>{extra?.name} {formatFromMoney(extra?.price)}</div>
             ))}
           </div>
         )}
         {product?.flavorsPrices?.length > 0 && (
           <div className="text-sm text-gray-500">
-            {product.flavorsPrices.map(flavor => (
+            {product?.flavorsPrices.map(flavor => (
               <div key={flavor?.name}>{flavor?.name} {formatFromMoney(flavor?.price)}</div>
             ))}
           </div>

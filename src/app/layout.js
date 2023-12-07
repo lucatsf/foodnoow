7c8bc23ec50ba1f8068f4fbc40ff1362ed3cf88c
Lucas Torres 🥷 <lucatsf@icloud.com>
2023-12-06 22:36:09 -0300

    Add Analytics component

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Header from "@/components/layout/Header";
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import {Toaster} from "react-hot-toast";
+import Analytics from "@/components/Analytics";
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
           <AppProvider>
+            <Analytics />
             <Toaster />
             <Header />
             {children}

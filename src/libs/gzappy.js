5e9ea4f796f6d16c60efafbfe9a7dc6dd0024e2e
Lucas Torres 🥷 <lucatsf@icloud.com>
2023-12-07 20:23:32 -0300

    Refactor gzappy function to handle development environment

--- src/libs/gzappy.js
+++ src/libs/gzappy.js
@@ -1,22 +1,25 @@
 export const gzappy = async ({message, phone}) => {
-
-  if (process.env.NEXT_NODE_ENV === 'development') {
-    return {success: true}
-  }
-  const response = await fetch(process.env.NEXT_GZAPPY_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      "user_token_id": process.env.NEXT_GZAPPY_USER_TOKEN_ID,
-    },
-    body: JSON.stringify({
-      instance_id: process.env.NEXT_GZAPPY_INSTANCE_ID,
-      instance_token: process.env.NEXT_GZAPPY_INSTANCE_TOKEN,
-      message,
-      phone: 55+phone,
+  try {
+    if (process.env.NEXT_NODE_ENV === 'development') {
+      return {success: true}
+    }
+    const response = await fetch(process.env.NEXT_GZAPPY_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "user_token_id": process.env.NEXT_GZAPPY_USER_TOKEN_ID,
+      },
+      body: JSON.stringify({
+        instance_id: process.env.NEXT_GZAPPY_INSTANCE_ID,
+        instance_token: process.env.NEXT_GZAPPY_INSTANCE_TOKEN,
+        message,
+        phone: 55+phone,
+      })
     })
-  })
 
-  const data = await response.json()
-  return data
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error);
+  }
 }
